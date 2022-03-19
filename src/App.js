63061bbded32bc9a23b60de9159b591bb44bd392
shuyin21 <shuyindev@gmail.com>
2022-03-19 17:46:36 +0000

    services element created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Homepage from './pages/Homepage';
 import './App.css';
 import Nav from './components/Nav';
 import Sidebar from './components/Sidebar';
+import Services from './pages/Services';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Homepage />} />
 
+          <Route path='/services' element={<Services />} />
           <Route path='/contact' element={<Homepage />} />
         </Routes>
       </Router>
